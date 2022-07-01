fe14085a7975eabd8db7847047e324122b2d7ff2
nozymech <sjyang.pt@gmail.com>
2022-07-01 22:14:42 +0800

    Add input title,author,page,status function

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let library = ["The Happiness Project","The Happiness Project","The Happiness Project"]
+let library = [{title:"The Happiness Project", author:"Gretchen Rubin",page:"368",read:"read"}];
 
 let list =
 document.getElementById("library")
 let title = document.getElementById("title")
 let author = document.getElementById("author")
 let page = document.getElementById("page")
+let gridCheck = document.getElementById("gridCheck")
+console.log(gridCheck.checked);
 //抓到html元素
 
-const randerlist=() => {
+const randerList=() => {
   let result = '';
 for (let i=0; i<library.length;i++) {result = result +`<div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${library[i]}</h5>
+    <h5 class="card-title">${library[i].title}</h5>
     <p class="card-text">
-      <div>by Gretchen Rubin </div>
-      <div>total 367 pages</div>
-      <div>status: read</div>
+      <div>by ${library[i].author} </div>
+      <div>total ${library[i].page} pages</div>
+      <div>status: ${library[i].read}</div>
     </p>
     <a href="#" class="btn btn-danger"  onclick="deleteBook(${i})">Delete</a>
   </div>
@@ -23,20 +25,24 @@ for (let i=0; i<library.length;i++) {result = result +`<div class="card" style="
   return result;
 }//使array 轉換成html: 按鈕＋元素
 
-list.innerHTML=randerlist();
+list.innerHTML=randerList();
 
 const addintoLibrary = () => {
-  if(title.value == "") {
-  }else{
-      library.push(title.value);
+  let book = {
+    title: title.value,
+    author: author.value,
+    page: page.value,
+    read: gridCheck.checked ? "Read" : "Not Read"
   }
-  title.value="";
- list.innerHTML=randerlist();//reloading list
+  library.push(book);
+  document.querySelector('form').reset();
+  list.innerHTML=randerList();//reloading list
+  localStorage.setItem('myBookList',JSON.stringify(library) )
 }
 
 const deleteBook = (i) => {
   library.splice(i,1);
-  list.innerHTML= randerlist();
+  list.innerHTML= randerList();
 }
 
 class Books{
